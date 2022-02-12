90dd16208846ee67c93f39152009d926b626e0d6
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-02-12 22:29:53 +0530

    build stripe gateway

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,13 +1,9 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-    },
+    { source: req.body.tokenId, amount: req.body.amount, currency: "usd" },
     (stripeErr, stripeRes) => {
       if (stripeErr) {
         res.status(500).json(stripeErr);
